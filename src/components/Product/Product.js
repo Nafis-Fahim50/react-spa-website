0ab2a1a7f1186bcd04f9e148dc53437e58309b62
Nafis-Fahim <nafisf96@gmail.com>
2022-09-25 13:12:16 +0600

    added product list

---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {img,name,price,seller,ratings} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p>Price: ${price}</p>
+            <br />
+            <p>Manufacturer: {seller}</p>
+            <p>Ratings: {ratings} star</p>
+            </div>
+            
+            <button className='btn-cart'>
+            <p>Add to cart</p>
+            </button>
+            
+        </div>
+    );
+};
+
+export default Product;
